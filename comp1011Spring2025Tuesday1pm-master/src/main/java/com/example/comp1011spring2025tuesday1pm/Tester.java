2dd960ac763c2452d239ddbea6269e48703b4b04
Niall Whelan <200600787@student.georgianc.on.ca>
2025-05-27 14:53:42 -0400

    Delete comp1011Spring2025Tuesday1pm-master directory

--- comp1011Spring2025Tuesday1pm-master/src/main/java/com/example/comp1011spring2025tuesday1pm/Tester.java
+++
@@ -1,9 +0,0 @@
-package com.example.comp1011spring2025tuesday1pm;
-
-public class Tester {
-
-    public static void main(String[] args) {
-        CameraModel model = new CameraModel();
-        model.Memory = CameraModel.MemoryOptions.GB_32;
-    }
-}
--- comp1011Spring2025Tuesday1pm-master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.comp1011spring2025tuesday1pm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.comp1011spring2025tuesday1pm to javafx.fxml;
-    exports com.example.comp1011spring2025tuesday1pm;
-}
