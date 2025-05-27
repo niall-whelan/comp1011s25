ca6c5bd58eefa23162f4e256626bb12939118435
niall-whelan <200600787@student.georgianc.on.ca>
2025-05-27 17:09:48 -0400

    5:09pm commit for helpfulness

---
+++ comp1011Spring2025Tuesday1pm/src/main/java/com/example/comp1011spring2025tuesday1pm/QuizGameController.java
@@ -0,0 +1,21 @@
+package com.example.comp1011spring2025tuesday1pm;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+public class QuizGameController {
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private ComboBox<String> cbNickname;
+
+    @FXML
+    public void initalize() {
+
+    }
+}
